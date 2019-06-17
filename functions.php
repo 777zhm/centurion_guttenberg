ab943e10da9e522a7d2cc2c4dd5921f988d0d262
Yuriy Zhmurko <777zhm@gmail.com>
2019-06-17 22:44:32 +0300

    deadline is comming

--- functions.php
+++ functions.php
@@ -6,4 +6,14 @@ function enqueue_parent_styles() {
 }
 
 
+// function my_scripts_method() {
+//     wp_enqueue_script(
+//         'custom',
+//         get_stylesheet_directory_uri() . '/assets/js/custom.js'
+//     );
+// }
+
+// add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+
+
 ?>
